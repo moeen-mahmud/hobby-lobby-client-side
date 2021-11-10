f671dc60a1e1b43d2946d79c2f0531f48c5134dd
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-10 11:40:45 +0600

    added primary components

---
+++ src/components/ProductContainer/ProductContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductContainer = () => {
+  return (
+    <div>
+      <h1>This is product container</h1>
+    </div>
+  );
+};
+
+export default ProductContainer;
