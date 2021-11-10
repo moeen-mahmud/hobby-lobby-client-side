6a0f7ef30c6233abf60ef7b73ceff53866bf6088
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-10 11:49:12 +0600

    added basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Explore from "./Pages/Explore/Explore";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Purchase from "./Pages/Purchase/Purchase";
+import Register from "./Pages/Register/Register";
+import NotFound from "./Pages/NotFound/NotFound";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Main App Component</h1>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/explore">
+            <Explore></Explore>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/register">
+            <Register></Register>
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard></Dashboard>
+          </Route>
+          <Route path="/purchase">
+            <Purchase></Purchase>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
