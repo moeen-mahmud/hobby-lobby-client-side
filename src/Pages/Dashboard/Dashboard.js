3e59b4b1227c94b765eee630f80d64381d21411b
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-11 11:48:46 +0600

    added user router in dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -21,7 +21,10 @@ import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import RateReviewIcon from "@mui/icons-material/RateReview";
 import LogoutIcon from "@mui/icons-material/Logout";
 import HomeIcon from "@mui/icons-material/Home";
-import { useHistory, useRouteMatch } from "react-router";
+import { useHistory, useRouteMatch, Switch, Route } from "react-router";
+import UserOrders from "../User/UserOrders/UserOrders";
+import Pay from "../User/Pay/Pay";
+import UserReviews from "../User/UserReviews/UserReviews";
 
 const drawerWidth = 220;
 
@@ -51,19 +54,20 @@ function Dashboard(props) {
         Back to home
       </Button>
       <List>
+        {/* Will add a active class like nanote later */}
         <ListItem onClick={() => history.push(`${url}`)} button>
           <ListItemIcon>
             <ShoppingBasketIcon style={{ color: "#4caf50" }} />
           </ListItemIcon>
           <ListItemText primary="My Orders" />
         </ListItem>
-        <ListItem button>
+        <ListItem onClick={() => history.push(`${url}/pay`)} button>
           <ListItemIcon>
             <PaymentIcon style={{ color: "#4caf50" }} />
           </ListItemIcon>
           <ListItemText primary="Pay" />
         </ListItem>
-        <ListItem button>
+        <ListItem onClick={() => history.push(`${url}/review`)} button>
           <ListItemIcon>
             <RateReviewIcon style={{ color: "#4caf50" }} />
           </ListItemIcon>
@@ -155,35 +159,17 @@ function Dashboard(props) {
         }}
       >
         <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Switch>
+          <Route exact path={path}>
+            <UserOrders></UserOrders>
+          </Route>
+          <Route path={`${path}/pay`}>
+            <Pay></Pay>
+          </Route>
+          <Route path={`${path}/review`}>
+            <UserReviews></UserReviews>
+          </Route>
+        </Switch>
       </Box>
     </Box>
   );
