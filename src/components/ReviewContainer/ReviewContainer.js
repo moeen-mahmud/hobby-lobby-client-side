b7ef2642bec341100de70fbfa5e69d38c559dfb8
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-12 21:14:39 +0600

    refactored review container

--- src/components/ReviewContainer/ReviewContainer.js
+++ src/components/ReviewContainer/ReviewContainer.js
@@ -1,12 +1,22 @@
+// React and necessary hooks
+import React, { useEffect, useState } from "react";
+
+// Modules from Material UI
 import { Container, Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+
+// Axios for data fetching
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+
+// Review component
 import Review from "../Review/Review";
 
+// Main Review Container Component
 const ReviewContainer = () => {
+  // Storing the reviews in the state
   const [reviews, setReviews] = useState([]);
 
+  // Fetching data from the API
   useEffect(() => {
     axios
       .get("https://morning-scrubland-84603.herokuapp.com/reviews")
@@ -17,6 +27,7 @@ const ReviewContainer = () => {
 
   return (
     <Container sx={{ mb: 15 }}>
+      {/* Section title */}
       <Typography
         variant="h4"
         component="h3"
@@ -25,6 +36,7 @@ const ReviewContainer = () => {
         Our Satisfied Customers Say...
       </Typography>
       <Box>
+        {/* Map through the reviews data */}
         <Grid container columns={{ xs: 1, md: 12 }} spacing={{ xs: 1, md: 6 }}>
           {reviews.map((review) => (
             <Review key={review._id} review={review}></Review>
