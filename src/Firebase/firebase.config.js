52bffa9f00184f30cbc994f7dc5d29ef68aab96e
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-12 21:21:07 +0600

    refactored firebase config files

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,3 +1,4 @@
+// Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -1,6 +1,10 @@
+// From starting the Firebase App
 import { initializeApp } from "firebase/app";
+
+// Getting the Firebase configuration
 import firebaseConfig from "./firebase.config";
 
+// Initialize Firebase Authentication
 const initializeAuthentication = () => {
   initializeApp(firebaseConfig);
 };
