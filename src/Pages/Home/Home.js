a832c18128848f914310a2c73adfa0b50a7519a4
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-13 00:04:52 +0600

    refactored home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,10 @@
+// React
 import React from "react";
 
+// Module from Material UI
 import { Box } from "@mui/system";
 
+// Necessary components
 import Navbar from "../../components/Navbar/Navbar";
 import Banner from "../../components/Banner/Banner";
 import ProductContainer from "../../components/ProductContainer/ProductContainer";
@@ -9,6 +12,7 @@ import ReviewContainer from "../../components/ReviewContainer/ReviewContainer";
 import About from "../../components/About/About";
 import Footer from "../../components/Footer/Footer";
 
+// Main Home Component
 const Home = () => {
   return (
     <Box>
