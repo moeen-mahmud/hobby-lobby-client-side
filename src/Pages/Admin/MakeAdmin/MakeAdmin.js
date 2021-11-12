242c2579c6fa0848f3f333b5801442b7f790409b
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-12 23:35:02 +0600

    refactored make admin

--- src/Pages/Admin/MakeAdmin/MakeAdmin.js
+++ src/Pages/Admin/MakeAdmin/MakeAdmin.js
@@ -1,3 +1,4 @@
+// Modules from Material UI
 import {
   Alert,
   Button,
@@ -8,36 +9,47 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+
+// Axios
 import axios from "axios";
+
+// React and necessary hook
 import React, { useState } from "react";
 
+// Main Make Admin Component
 const MakeAdmin = () => {
+  // Store the email in the state
   const [email, setEmail] = useState("");
+
+  // State for Snackbar
   const [openSnackBar, setOpenSnackBar] = useState(false);
 
+  // Functionality for making an admin
   const handleMakeAdmin = (e) => {
     e.preventDefault();
 
+    // Set email
     const user = { email };
     axios
       .put("https://morning-scrubland-84603.herokuapp.com/users", user)
       .then((res) => {
         if (res.data.modifiedCount > 0) {
-          handleOpenSnackBar();
+          handleOpenSnackBar(); // Show Snackbar for successfully modified count
         }
       });
   };
 
+  // Function for handling snackbar
   const handleOpenSnackBar = () => {
     setOpenSnackBar(true);
   };
-
   const handleCloseSnackBar = () => {
     setOpenSnackBar(false);
   };
 
   return (
     <Container>
+      {/* Snackbar component */}
       <Snackbar
         open={openSnackBar}
         autoHideDuration={6000}
@@ -53,10 +65,12 @@ const MakeAdmin = () => {
       </Snackbar>
       <Grid container columns={{ xs: 1, md: 12 }}>
         <Grid item xs={12} md={5}>
+          {/* Section title */}
           <Typography sx={{ mb: 3 }} variant="h4">
             Make an admin
           </Typography>
           <form onSubmit={handleMakeAdmin}>
+            {/* Getting information */}
             <Stack direction="column" spacing={3}>
               <TextField
                 variant="outlined"
