8d91227add879fcf9813bc9391c4bf82b5ccb62d
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-12 21:02:34 +0600

    refactored product section

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,13 +1,23 @@
+// React
+import React from "react";
+
+// Modules from Material UI
 import { Button, Paper, Rating, Stack, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+
+// Importing axios
 import axios from "axios";
-import React from "react";
+
+// Hooks from React Router
 import { useHistory, useLocation } from "react-router";
 
+// Main Product Component
 const Product = ({ product }) => {
+  // Using the hooks
   const location = useLocation();
   const history = useHistory();
 
+  // Destructuring values from props
   const {
     _id,
     productName,
@@ -18,6 +28,7 @@ const Product = ({ product }) => {
     productRating,
   } = product;
 
+  // Function for handling the order
   const handleOrder = (id) => {
     axios
       .get(`https://morning-scrubland-84603.herokuapp.com/products/${id}`)
@@ -31,15 +42,18 @@ const Product = ({ product }) => {
       sx={{ p: 3, border: "2px solid #4caf50", borderRadius: "5px" }}
       elevation={0}
     >
+      {/* Product image */}
       <img
         style={{ width: "80%", marginBottom: "2rem" }}
         src={productImage}
         alt={productName}
       />
+      {/* Product rating */}
       <Stack direction="row" spacing={2} alignItems="center">
         <Typography variant="body1">Rating:</Typography>
         <Rating precision={0.1} value={productRating} readOnly />
       </Stack>
+      {/* Product information */}
       <Box sx={{ textAlign: "left", mb: 3 }}>
         <Typography sx={{ mt: 2 }} variant="h6" component="h3">
           {productName}
@@ -48,6 +62,7 @@ const Product = ({ product }) => {
           {productShorDesc}
         </Typography>
       </Box>
+      {/* Render the description if the route match with the following route */}
       {location.pathname === "/explore" && (
         <Box sx={{ mb: 2, textAlign: "left" }}>
           <Typography variant="body2" color="text.secondary">
@@ -60,11 +75,13 @@ const Product = ({ product }) => {
             </Typography>{" "}
             {productLongDesc}
           </Typography>
+          {/* Product price */}
           <Typography sx={{ fontWeight: 700, mt: 3 }} variant="h6">
             Price: ${productPrice}
           </Typography>
         </Box>
       )}
+      {/* Order button */}
       <Stack>
         <Button
           onClick={() => handleOrder(_id)}
