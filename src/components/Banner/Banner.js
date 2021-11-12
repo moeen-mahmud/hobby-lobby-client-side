ffc52d3714e45d753506015e31c36dfbb04b69bb
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-12 19:59:22 +0600

    made banner responsive

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 // Material
-import { Typography } from "@mui/material";
+import { Typography, useMediaQuery } from "@mui/material";
 import { Box } from "@mui/system";
 
 // Banner Image
 import bgBanner from "../../assets/bg-banner-main.png";
+import { useTheme } from "@emotion/react";
 
 // Banner style
 const bgStyle = {
@@ -17,6 +18,9 @@ const bgStyle = {
 };
 
 const Banner = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <Box
       style={bgStyle}
@@ -26,35 +30,72 @@ const Banner = () => {
       }}
     >
       <Box sx={{ textAlign: "center", mt: 15 }}>
-        <Typography
-          variant="h2"
-          sx={{
-            fontWeight: 700,
-            textShadow: "1px 1px #232832",
-            lineHeight: "1.5",
-            mb: 3,
-          }}
-          component="h1"
-          color="secondary.main"
-        >
-          It's Really Important <br /> to Have A Good Gaming Chair
-        </Typography>
-        <Typography
-          variant="body1"
-          component="p"
-          color="neutral.main"
-          sx={{
-            textShadow: "1px 1px #232832",
-            fontSize: "1.2rem",
-            lineHeight: "2",
-            fontWeight: 500,
-          }}
-        >
-          We are always overlooking the most importanct factor while gaming.
-          <br />
-          A good gaming chair helps us to mantain our postures. <br /> It's
-          really important for those who spend a long session in sitting.
-        </Typography>
+        {isMobile ? (
+          <>
+            <Typography
+              variant="h5"
+              sx={{
+                fontWeight: 700,
+                textShadow: "1px 1px #232832",
+                lineHeight: "1.5",
+                mb: 3,
+              }}
+              component="h1"
+              color="secondary.main"
+            >
+              It's Really Important <br /> to Have A Good Gaming Chair
+            </Typography>
+            <Typography
+              variant="body1"
+              component="p"
+              color="neutral.main"
+              sx={{
+                color: "#81c784",
+                textAlign: "justify",
+                textShadow: "1px 1px #232832",
+                lineHeight: "2",
+                px: 1,
+                fontWeight: 500,
+              }}
+            >
+              We are always overlooking the most importanct factor while gaming.
+              A good gaming chair helps us to mantain our postures. It's really
+              important for those who spend a long session in sitting.
+            </Typography>
+          </>
+        ) : (
+          <>
+            <Typography
+              variant="h2"
+              sx={{
+                fontWeight: 700,
+                textShadow: "1px 1px #232832",
+                lineHeight: "1.5",
+                mb: 3,
+              }}
+              component="h1"
+              color="secondary.main"
+            >
+              It's Really Important <br /> to Have A Good Gaming Chair
+            </Typography>
+            <Typography
+              variant="body1"
+              component="p"
+              color="neutral.main"
+              sx={{
+                textShadow: "1px 1px #232832",
+                fontSize: "1.2rem",
+                lineHeight: "2",
+                fontWeight: 500,
+              }}
+            >
+              We are always overlooking the most importanct factor while gaming.
+              <br />
+              A good gaming chair helps us to mantain our postures. <br /> It's
+              really important for those who spend a long session in sitting.
+            </Typography>
+          </>
+        )}
       </Box>
     </Box>
   );
