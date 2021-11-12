11c3f58f9beb882a03df1bdc757171441cf3a7ca
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-12 17:28:46 +0600

    added notfound page

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,10 +1,34 @@
+import { Button, Container, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import { useHistory } from "react-router";
 
 const NotFound = () => {
+  const history = useHistory();
+
   return (
-    <div>
-      <h1>404 | Page not found!</h1>
-    </div>
+    <Container>
+      <Box sx={{ textAlign: "center", mt: 10 }}>
+        <Typography
+          sx={{ fontWeight: 700, color: "secondary.main" }}
+          variant="h1"
+        >
+          404
+        </Typography>
+        <Typography variant="h4">Page not found</Typography>
+        <Typography mt={3} color="text.secondary" variant="subtitle1">
+          Let me guess, you did something with the url... 🤔
+        </Typography>
+        <Button
+          onClick={() => history.push("/")}
+          sx={{ mt: 2 }}
+          variant="outlined"
+          color="secondary"
+        >
+          Take me where I belong
+        </Button>
+      </Box>
+    </Container>
   );
 };
 
