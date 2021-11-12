b6b97cfab977b2dcc241223726ff53870c46ed8c
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-13 00:12:34 +0600

    refactored NotFound page

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,13 +1,21 @@
+// Modules from Material UI
 import { Button, Container, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+
+// React
 import React from "react";
+
+// Hooks from React Router
 import { useHistory } from "react-router";
 
+// Main Not Found Component
 const NotFound = () => {
+  // Calling the hook
   const history = useHistory();
 
   return (
     <Container>
+      {/* Contents */}
       <Box sx={{ textAlign: "center", mt: 10 }}>
         <Typography
           sx={{ fontWeight: 700, color: "secondary.main" }}
@@ -19,6 +27,7 @@ const NotFound = () => {
         <Typography mt={3} color="text.secondary" variant="subtitle1">
           Let me guess, you did something with the url... 🤔
         </Typography>
+        {/* For going back to home */}
         <Button
           onClick={() => history.push("/")}
           sx={{ mt: 2 }}
